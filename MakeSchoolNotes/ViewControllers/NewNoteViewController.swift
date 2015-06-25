9d8bb742cde552da64439c4a24e2273ffaeb961e
Martin Walsh <martin.walsh@gmail.com>
2015-06-25 15:39:37 -0700

    Solution Added

---
+++ MakeSchoolNotes/ViewControllers/NewNoteViewController.swift
@@ -0,0 +1,42 @@
+//
+//  NewNoteViewController.swift
+//  MakeSchoolNotes
+//
+//  Created by Martin Walsh on 02/06/2015.
+//  Copyright (c) 2015 MakeSchool. All rights reserved.
+//
+
+import UIKit
+
+class NewNoteViewController: UIViewController {
+    
+    var currentNote: Note?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        
+        if (segue.identifier == "ShowNewNote") {
+            // create a new Note and hold onto it, to be able to save it later
+            currentNote = Note()
+            let noteViewController = segue.destinationViewController as! NoteDisplayViewController
+            noteViewController.note = currentNote
+            noteViewController.edit = true
+        }
+    }
+
+}
